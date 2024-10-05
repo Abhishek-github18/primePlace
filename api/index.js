3a1407e5eae7741636ed77bc6ced31148ab35cca
unknown <abhishekrock796@gmail.com>
2024-10-05 16:11:03 +0530

    db configured

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongo_url = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
