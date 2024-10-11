ae2c73afcfdfaf6fe1cc3c9c556e7b675660325e
unknown <abhishekrock796@gmail.com>
2024-10-11 09:24:52 +0530

    login functionality done

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,28 +1,24 @@
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   try {
     console.log("req.body", req.body);
     const salt = process.env.Salt;
     const { username, email, password } = req.body;
-    let hashedPassword = "";
+    // let hashedPassword = "";
 
-    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, 10);  // Make sure password is valid before hashing
 
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-      bcrypt.hash(password, salt, function (err, hash) {
-        if (err) {
-          next(errorHandler(500, "Internal server error"));
-        }
-        hashedPassword = hash;
-      });
-    });
+    
+    console.log("hashedPassword", hashedPassword);
 
     const user = await User.create({
       username,
       email,
-      password,
+      "password": hashedPassword,
     });
     console.log("User created in cosmos db", user);
     res.status(201).json({
@@ -31,9 +27,52 @@ export const signup = async (req, res, next) => {
     });
   } catch (err) {
     console.log("Error", err);
-    if(err.code === 11000){
-        next(errorHandler(409, "Username or Email already exists"));
+    if (err.code === 11000) {
+      next(errorHandler(409, "Username or Email already exists"));
     }
     next(errorHandler(500, "Internal server error"));
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const validUser = await User.findOne({ username });
+  if (!validUser) {
+    next(errorHandler(404, "User not registered"));
+    return;
+  }
+  console.log("validUser", validUser);
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      if (err) {
+        next(errorHandler(500, "Internal server error"));
+      }
+     console.log(hash);
+    });
+  });
+  bcrypt.compare(password, validUser.password, function (err, result) {
+    if (err) {
+      console.log("Error while comparing signin password", err);
+      next(errorHandler(500, "Internal server error"));
+    } else {
+      if (result) {
+        const token = jwt.sign(
+          {
+            id: validUser._id,
+          },
+          process.env.SecretKey,
+          { expiresIn: "1h" }
+        );
+        res.cookie("token", token, { httpOnly: true }).status(200).json({
+          status: true,
+          username: validUser.username,
+          id: validUser._id,
+        });
+      } else {
+        next(errorHandler(401, "Invalid credentials"));
+        return;
+      }
+    }
+  });
 };
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,89 @@
-const Login = () => {
-  return (
-    <div>
+import { useRef, useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate, Link } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import MessageReactToast from "../utils/MessageReactToast";
 
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event)=>{
+    event.preventDefault();
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+
+    setLoading(true);
+
+    try{
+      const response = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      if(!response.ok){
+        const error = await response.json();
+        throw new Error(error.message);
+      }else{
+        toast.success(
+          <MessageReactToast message={"Logged In Successfully"} closeToast={() => toast.dismiss()} />
+        );
+        setLoading(false);  
+        navigate("/profile");
+      }
+    }catch(error){
+      toast.error(<MessageReactToast message={error.message} closeToast={() => toast.dismiss()} />)
+      setLoading(false);
+    }
+    console.log(username, password);
+  }
+
+  return (
+    <div className="bg-white flex items-center justify-center mt-16">
+    <div className="w-full max-w-lg p-6 sm:p-12 bg-gray-100 shadow-md rounded-lg">
+      <h1 className="text-3xl font-bold text-blue-900 mb-8 text-center">
         Login
-      
+      </h1>
+
+      <form className="flex flex-col gap-4 mb-6" onSubmit={handleSubmit}>
+        <input
+          className="p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-teal-500"
+          type="text"
+          placeholder="Username"
+          ref={usernameRef}
+          required
+        />
+      <input
+          className="p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-teal-500"
+          type="password"
+          placeholder="Password"
+          ref={passwordRef}
+          required
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-4 rounded-lg hover:bg-teal-500 hover:animate-wobble transition duration-300"
+        >
+          {loading ? "Loading...." : "Login"}
+        </button>
+      </form>
+
+      {/* Already have an account section */}
+      <div className="text-center">
+        <p className="text-gray-600 mb-4">Dont have an account?</p>
+        <Link to="/signup">
+          <button className="bg-teal-500 text-white py-2 px-6 rounded-lg hover:bg-teal-600 transition duration-300">
+            Signup
+          </button>
+        </Link>
+      </div>
     </div>
+  </div>
   )
 }
 
