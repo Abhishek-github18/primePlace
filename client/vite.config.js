cc0bf56c81e7f06dc1ca24b9ac7235d979af16da
unknown <abhishekrock796@gmail.com>
2024-12-08 19:40:27 +0530

    removed poxy config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://primeplace-pr6i.onrender.com", // Replace with the correct remote API URL
-        changeOrigin: true, // Ensure the Host header is correctly set
-        secure: true, // If the target server uses HTTPS, set to true
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: strip /api from the request URL if necessary
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://primeplace-pr6i.onrender.com", // Replace with the correct remote API URL
+  //       changeOrigin: true, // Ensure the Host header is correctly set
+  //       secure: true, // If the target server uses HTTPS, set to true
+  //       rewrite: (path) => path.replace(/^\/api/, ''), // Optional: strip /api from the request URL if necessary
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
