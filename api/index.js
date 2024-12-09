32931423029c69a4308dbc58a3a3575b720b19a0
unknown <abhishekrock796@gmail.com>
2024-12-09 22:39:30 +0530

    backend config for cookie

--- api/index.js
+++ api/index.js
@@ -24,21 +24,26 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const allowedOrigins = ['http://localhost:5173', 'https://prime-place.vercel.app'];
   const origin = req.headers.origin;
 
-  // Set CORS headers only for allowed origins
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow cookies with credentials
   }
   
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Enable cookies with cross-origin requests
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, UPDATE, OPTIONS');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
   next();
 });
+
 
 
 app.use("/api/health", healthRouter);
