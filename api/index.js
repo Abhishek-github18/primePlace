65b5ffb88985ca87bcb94cea281fcab9d7f9f47e
unknown <abhishekrock796@gmail.com>
2024-12-10 20:06:30 +0530

    cors package config

--- api/index.js
+++ api/index.js
@@ -24,7 +24,13 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://prime-place.vercel.app"], // Allow these origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
 
 
 
